a3bb7ae58bbb4a066b3e540bd3dee70943c4582a
nurayklc <nuraykilic0101@gmail.com>
2022-07-01 16:24:22 +0300

    Add  category section in admin page

--- MvcProjectCamp/Controllers/AdminCategoryController.cs
+++ MvcProjectCamp/Controllers/AdminCategoryController.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +14,36 @@ namespace MvcProjectCamp.Controllers
 {
     public class AdminCategoryController : Controller
     {
+        CategoryManager categorManager = new CategoryManager(new EFCategoryDAL());
+
         // GET: AdminCategory
         public ActionResult Index()
         {
+            var categoryList = categorManager.GetCategories();
+            return View(categoryList);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]  
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult result =  categoryValidator.Validate(category);
+            if (result.IsValid)
+            {
+                categorManager.AddCategory(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
