b748a8ab5ea21b096e5a9d2932e545c6f23f0ec9
nurayklc <nuraykilic0101@gmail.com>
2022-07-18 15:46:03 +0300

    Complete second homework

---
+++ MvcProjectCamp/Controllers/StaticsController.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class StaticsController : Controller
+    {
+        // GET: Statics
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var totalCategory = context.Categories.Count();
+            ViewBag.TotalCategory = totalCategory;
+
+            var subTitleCategory = context.Headings.Where(x => x.CategoryID == 17).Count();
+            ViewBag.SubTitleCategory = subTitleCategory;
+
+            var indexOfCharWithAuthor = context.Authors.Count(x => x.AuthorName.Contains("a"));
+            ViewBag.IndexOfCharWithAuthor = indexOfCharWithAuthor;
+
+            var mostTitle = context.Categories.Max(x => x.Headings.Count);
+            ViewBag.MostTitle = mostTitle;
+
+            var differentCategoryStatus = context.Categories.Where(x => x.CategoryStatus == true).Count() - context.Categories.Where(x => x.CategoryStatus == false).Count();
+            ViewBag.DifferentCategoryStatus = differentCategoryStatus;
+
+            return View();
+        }
+    }
+}
